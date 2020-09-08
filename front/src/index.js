c25f63725f46a4ebd06d5e7f6f216d6c1b646b95
ayoub <ayoub.belkacemi@epitech.eu>
2020-09-08 16:09:15 +0200

    [Done] Mission sans bonus

---
+++ front/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'w3-css/w3.css';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
